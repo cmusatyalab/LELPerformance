2e62397a3f4ed4989811445089522a0a97130ba2
Jim Blakley <jblake1@andrew.cmu.edu>
2022-08-16 19:09:34 +0000

    use local_common

--- processing/waterspout_measure.py
+++ processing/waterspout_measure.py
@@ -13,6 +13,8 @@ from influxdb import InfluxDBClient
 # Logging
 import simlogging
 from simlogging import mconsole, logging
+from local_common import createDB,getDBs
+
 
 def main():
     global IP_ADDR
@@ -131,31 +133,6 @@ def log_packet(pkt):
         icmp_client.write_points(packets)
 
 
-def getDBs(client):
-    response = client.query("show databases")
-    try:
-        dbs = [db[0] for db in response.raw['series'][0]['values']]
-        return dbs
-    except:
-        mconsole("Could not parse influxdb database list: {}".format(response),level="ERROR")
-    return None
-
-def createDB(client,dbname):
-    dbs = getDBs(client)
-    if dbs is not None and len(dbs) > 0 and dbname in dbs:
-        ''' Check openrtistdb influxdb '''
-        mconsole("Database {} exists in influxdb".format(dbname))
-        return True
-    else:
-        mconsole("Creating database {} in influxdb".format(dbname))
-        client.create_database(dbname)
-        client.alter_retention_policy("autogen", database=dbname, duration="30d", default=True)
-        client.create_retention_policy('DefaultRetentionPolicy', '365d', "3", database = dbname, default=True)
-        dbs = getDBs(client)
-        if not dbname in dbs:
-            mconsole("Could not create: {}".format(DBNAME),level="ERROR")
-            return False
-    return True
 
 if __name__ == '__main__': main()
 
