9062b1200417ab0346bcda4771bc4afe66f4b0e7
Jim Blakley <jblake1@andrew.cmu.edu>
2022-08-17 13:43:34 +0000

    use config.json

--- processing/cleanDBs.py
+++ processing/cleanDBs.py
@@ -7,12 +7,9 @@ import os
 from influxdb import InfluxDBClient, DataFrameClient
 import numpy as np
 import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
 import time
 from pyutils import *
 from pdutils import *
-from pdpltutils import *
 
 from optparse import OptionParser
 import simlogging
@@ -23,23 +20,10 @@ from simlogging import mconsole
 LOGNAME=__name__
 LOGLEV = logging.DEBUG
 
-# Hardcode cloudlet IP and port for DB
-CLOUDLET_IP = '128.2.208.248'
-EPC_IP = '192.168.25.4'
-CLOUDLET_PORT = 8086
-TZ = 'America/New_York'
-''' Obtain clients for ICMP databases ''' 
-CLOUDLET_ICMP_DB = 'cloudleticmp'
-WATERSPOUT_ICMP_DB = 'waterspouticmp'
-UE_ICMP_DB = 'ueicmp'
-SEG_DB = 'segmentation'
-OFFSET_DB = 'winoffset'
-measuredict = {CLOUDLET_ICMP_DB:['latency'],UE_ICMP_DB:['latency'],WATERSPOUT_ICMP_DB:['latency']
-               ,SEG_DB:['uplink','downlink'],OFFSET_DB:['winoffset']}
-
 def main():
     global logger
-    LOGFILE="database_clean.log"
+    kwargs = configure()
+    
     logger = simlogging.configureLogging(LOGNAME=LOGNAME,LOGFILE=LOGFILE,loglev = LOGLEV,coloron=False)
     (options,_) = cmdOptions()
     kwargs = options.__dict__.copy()
@@ -54,8 +38,8 @@ def main():
             mconsole("OK, walking you through each measurement")
             kwargs['DELETEALL'] = False
         for db in measuredict.keys():
-            dfclient = DataFrameClient(host=CLOUDLET_IP, port=CLOUDLET_PORT, database=db)
-            wtclient = InfluxDBClient(host=CLOUDLET_IP, port=CLOUDLET_PORT, database=db)
+            dfclient = DataFrameClient(host=INFLUXDB_IP, port=INFLUXDB_PORT, database=db)
+            wtclient = InfluxDBClient(host=INFLUXDB_IP, port=INFLUXDB_PORT, database=db)
             for measure in measuredict[db]:
                 try:
                     tdfx = dfclient.query("select * from {}".format(measure))[measure]
@@ -73,8 +57,57 @@ def main():
 def deleteMeasure(db,measure,wtclient):
     mconsole("Dropping {}.{}".format(db,measure))
     wtclient.drop_measurement(measure)
+
+def configure():
+    global seg_client;global df_seg_client; global df_cloudlet_icmp_client
+    global df_waterspout_icmp_client;global df_ue_icmp_client
+    global TZ; global CLOUDLET_IP; global EPC_IP;
+    global INFLUXDB_IP; global INFLUXDB_PORT; global measuredict
+    global LOGFILE; global LOGNAME; global LOGLEV
+
+    LOGNAME=__name__
+    LOGLEV = logging.INFO
     
+    configfn = "config.json"
+    cnf = {}; ccnf = {};gcnf = {}
+    if configfn is not None and os.path.isfile(configfn):
+        with open(configfn) as f:
+            cnf = json.load(f)
+            qcnf = cnf['QUERY']
+            gcnf = cnf['GENERAL']
+            ucnf = cnf['UE']
+            ccnf = cnf['CLOUDLET']
+            wcnf = cnf['WATERSPOUT']
+    ''' General '''
+    key = "epc_ip"; EPC_IP = gcnf[key] if key in gcnf else "192.168.25.4"
+    key = "lelgw_ip"; LELGW_IP = gcnf[key] if key in gcnf else "128.2.212.53"
+    key = "cloudlet_ip" ; CLOUDLET_IP = gcnf[key] if key in gcnf else "128.2.208.248"
+    key = "ue_ip"; UE_IP = gcnf[key] if key in gcnf else "172.26.21.132"
+    key = "influxdb_port"; INFLUXDB_PORT = gcnf[key] if key in gcnf else 8086
+    key = "influxdb_ip"; INFLUXDB_IP = gcnf[key] if key in gcnf else CLOUDLET_IP
+    key = "timezone"; TZ = gcnf[key] if key in gcnf else "America/New_York"
+    key = "seg_db"; SEG_DB = gcnf[key] if key in gcnf else "segmentation"
+    key = "offset_db"; OFFSET_DB = gcnf[key] if key in gcnf else "winoffset"
     
+    ''' necessary node specific '''
+    LOGFILE= "database_cleanup.log"
+    key = "icmp_db"; CLOUDLET_ICMP_DB = ccnf[key] if key in ccnf else "cloudleticmp"
+    key = "icmp_db"; WATERSPOUT_ICMP_DB = wcnf[key] if key in wcnf else "cloudleticmp"
+    key = "icmp_db"; UE_ICMP_DB = ucnf[key] if key in ucnf else "ueicmp"
+    
+    measuredict = {CLOUDLET_ICMP_DB:['latency'],UE_ICMP_DB:['latency'],WATERSPOUT_ICMP_DB:['latency']
+               ,SEG_DB:['uplink','downlink'],OFFSET_DB:['winoffset']}
+     
+    ''' Get all the clients '''
+    seg_client = InfluxDBClient(host=INFLUXDB_IP, port=INFLUXDB_PORT, database=SEG_DB)
+    df_seg_client = DataFrameClient(host=INFLUXDB_IP, port=INFLUXDB_PORT, database=SEG_DB)
+    df_cloudlet_icmp_client = DataFrameClient(host=INFLUXDB_IP, port=INFLUXDB_PORT, database=CLOUDLET_ICMP_DB)
+    df_waterspout_icmp_client = DataFrameClient(host=INFLUXDB_IP, port=INFLUXDB_PORT, database=WATERSPOUT_ICMP_DB)
+    df_ue_icmp_client = DataFrameClient(host=INFLUXDB_IP, port=INFLUXDB_PORT, database=UE_ICMP_DB)
+    df_offset_client = DataFrameClient(host=INFLUXDB_IP, port=INFLUXDB_PORT, database=OFFSET_DB)
+    (options,_) = cmdOptions()
+    kwargs = options.__dict__.copy()
+    return kwargs
 
 def cmdOptions():
     parser = OptionParser(usage="usage: %prog [options]")
